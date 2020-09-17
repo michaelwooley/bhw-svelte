5e493f2ced704be5af5a3771476e4cb2ff445ec7
michaelwooley <michael@guhdoh.com>
2020-09-17 13:39:12 -0400

    Clean up main/preview files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  backgrounds: {
+    default: "app",
+    values: [
+      {
+        name: "app",
+        value: "#F5F5F5",
+      },
+    ],
+  },
+};
