ca96aacce7b30346ad7bff9d1125c7b777ad0d44
michaelwooley <michael@guhdoh.com>
2020-09-17 15:06:40 -0400

    init: Current weather column (display)

---
+++ src/components/CityCurrentWeather/index.stories.js
@@ -0,0 +1,23 @@
+import CityCurrentWeather from "./index.svelte";
+
+export default {
+  title: "City/CityCurrentWeather",
+  component: CityCurrentWeather,
+  argTypes: {
+    // isRight: { control: "boolean" },
+    // disabled: { control: "boolean" },
+    // handleRefresh: { action: "refresh" },
+    // handleMoveUp: { action: "move-up" },
+    // handleMoveDown: { action: "move-down" },
+    // handleRemove: { action: "remove" },
+  },
+};
+
+const Template = ({ ...args }) => ({
+  Component: CityCurrentWeather,
+  props: args,
+  on: {},
+});
+
+export const Main = Template.bind({});
+Main.args = {};
