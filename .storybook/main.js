52c2b703e8789d7ff4eaec6fc070fa3d42904f39
michaelwooley <michael@guhdoh.com>
2020-09-18 14:43:27 -0400

    Add console logging in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
+    "@storybook/addon-actions",
   ],
   webpackFinal: async (config) => {
     config.module.rules = config.module.rules
