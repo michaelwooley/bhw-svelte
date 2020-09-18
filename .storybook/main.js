9593e829ccf865e7daf6a949e424f44b3f17753b
michaelwooley <michael@guhdoh.com>
2020-09-18 16:06:49 -0400

    Add alias import in webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const { preprocess } = require("../svelte.config.js");
+const { preprocess, moduleResolver } = require("../svelte.config.js");
 
 // Export a function. Accept the base config as the only param.
 module.exports = {
@@ -20,6 +20,16 @@ module.exports = {
           options: { preprocess },
         },
       ]);
+
+    config.module.rules = config.module.rules.map((r) => {
+      if (!r.use) return r;
+      r.use = r.use.map((u) => {
+        if (u.loader !== "babel-loader") return u;
+        u.options.plugins = (u.options.plugins || []).concat([moduleResolver]);
+        return u;
+      });
+      return r;
+    });
 
     return config;
   },
