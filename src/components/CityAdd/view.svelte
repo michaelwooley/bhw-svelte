49b4053122cbb4af090b2f3f63b7591f024106ae
michaelwooley <michael@guhdoh.com>
2020-09-23 16:29:34 -0400

    Begin to add separate view for CityAdd

---
+++ src/components/CityAdd/view.svelte
@@ -0,0 +1,10 @@
+<script>
+  import CityAdd from "./index.svelte";
+  import type { IStationName } from "@app/common/data";
+
+  export let stations: IStationName[];
+</script>
+
+<div>
+  <CityAdd {stations} />
+</div>
