e51c8f258b577be8ab883d4104488d14a31c641e
michaelwooley <michael@guhdoh.com>
2020-09-23 16:52:48 -0400

    Consolidate box in CityModal, imply columns

--- src/components/CityModal/index.svelte
+++ src/components/CityModal/index.svelte
@@ -4,25 +4,26 @@
   }
 </style>
 
-<div class="modal-content">
-  <div class="box">
-    <article class="media">
-      <div class="media-content">
-        <h2 class="is-4 title">
-          <span class="icon"> <i class="fas fa-city" /> </span>
-          <span> Add, remove, and re-arrange your cities </span>
-        </h2>
-        <div class="body">
-          <slot name="search">Search/Add slot</slot>
-          <hr />
-          <slot name="current">Current cities slot</slot>
-        </div>
-        <div>
-          <span class="is-size-7 is-italic">To exit, press the <code>Escape</code>
-            key, click the background, or click the <span class="icon"><i
-                class="fas fa-delete" /></span> button in the top right.</span>
-        </div>
+<div class="modal-content box">
+  <article class="media">
+    <div class="media-content">
+      <h2 class="is-4 title">
+        <span class="icon"> <i class="fas fa-city" /> </span>
+        <span> Add, remove, and re-arrange your cities </span>
+      </h2>
+      <div class="body columns">
+        <slot name="search">
+          <div class="column is-one-quarter box">Search/Add slot</div>
+        </slot>
+        <slot name="current">
+          <div class="column">Current cities slot</div>
+        </slot>
       </div>
-    </article>
-  </div>
+      <div>
+        <span class="is-size-7 is-italic">To exit, press the <code>Escape</code>
+          key, click the background, or click the <span class="icon"><i
+              class="fas fa-delete" /></span> button in the top right.</span>
+      </div>
+    </div>
+  </article>
 </div>
